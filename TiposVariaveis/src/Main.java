b96404dd59eff6bc951a75deebadafc7f80e33a6
saviocunhaa <saviocunha.01@hotmail.com>
2024-04-30 10:55:29 -0300

    :sparkles: feat: criando tipo de variaveis

---
+++ TiposVariaveis/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // tipos de variaveis e java
+        String nomeCompleto = "Antonio Sávio Cunha de Sousa";
+        int telefone = 99990088;
+        float nota = 3.586F; // F no final do numero nomeclatura padrao
+        long numero = 9895654546565L;// L no final nomeclatura padrao
+
+        //VALORES CONTANTES - NAO PODE SER ALTERADAS
+        final String CPF = "000-000-000-00";
+        final double VALOR_PI = 3.14;
+
+    }
+}
